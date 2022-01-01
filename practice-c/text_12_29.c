93b366834ff828d0e5d26983c596a145a2645c7b
rhythmiccoder <96858446+rhythmiccoder@users.noreply.github.com>
2022-01-02 00:49:24 +0800

    Create text_12_29.c
    2

---
+++ practice-c/text_12_29.c
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_MO_WARNING
+//
+//#include <stdio.h>
+//
+//int a = 0;
+//int b = 0;
+//
+//void text()
+//{
+//	/*int a = 0;
+//	int b = 0;*/
+//	a++;
+//	b = a;
+//	while (b != 0)
+//	{
+//		printf("*");
+//		b--;
+//	}
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	while (m < 5)
+//	{
+//		m++;
+//		text();
+//		printf("\n");
+//	}
+//}
